32a7707bbc53f983c903986be02f3e4796a8e64a
danh221204 <danhpro123ok@gmail.com>
2022-10-05 16:33:22 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ThanhDanh-Project',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
